8c28c21c598e627425ca0f5c3b1da723479c160a
zia <ziahaqi.mail@gmail.com>
2016-01-12 16:49:02 +0700

    test ignoring

--- app/src/main/java/com/ziahaqi/mqsample/MainActivity.java
+++ app/src/main/java/com/ziahaqi/mqsample/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements MQConsumer.MQCons
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
