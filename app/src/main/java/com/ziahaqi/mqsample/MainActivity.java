fe16ed771c38715c715bf73e78d8e170a522f887
zia <ziahaqi.mail@gmail.com>
2016-02-04 16:24:12 +0700

    changes listner mechanism

--- app/src/main/java/com/ziahaqi/mqsample/MainActivity.java
+++ app/src/main/java/com/ziahaqi/mqsample/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements MQCallback{
     private MQProducer mqProducer;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
